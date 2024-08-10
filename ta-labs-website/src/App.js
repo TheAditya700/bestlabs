3e8f093dbfc51af4ecf9c93996d1af192a53baf1
TheAditya700 <adityahr700@gmail.com>
2024-08-10 10:52:15 +0530

    Initial commit - Set up React app with Tailwind CSS

---
+++ ta-labs-website/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* Add your header, hero, and main content here */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
